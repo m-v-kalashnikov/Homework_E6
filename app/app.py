8399b6d28bda7b6a6556e2e09f9b4e7ab98ef487
Kalashnikov Michael <49352386+m-v-kalashnikov@users.noreply.github.com>
2020-03-24 21:27:41 +0200

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+from app.fibonacci import cache_data, is_positiv_integer
+
+app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        number = request.form.get('number')
+        if is_positiv_integer(number):
+            return redirect(url_for('index_fibo', num=number))
+        else:
+            return redirect(url_for('index_fibo', num=-1))
+    else:
+        return render_template('index.html')
+
+
+@app.route('/<num>')
+def index_fibo(num):
+    number = cache_data(int(num))
+    return render_template('index.html', num=number)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=port)
